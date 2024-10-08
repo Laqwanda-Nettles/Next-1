88684ec6aec3890131586bd5e3a5ba1ca97f1382
Laqwanda Nettles <nettleslaqwanda@gmail.com>
2024-10-07 23:06:51 -0500

    created an about component

---
+++ src/pages/about/index.js
@@ -0,0 +1,25 @@
+export default function About() {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        inset: "0px",
+        width: "40rem",
+        height: "5rem",
+        maxWidth: "100vw",
+        maxHeight: "100dvh",
+        margin: "auto",
+      }}
+    >
+      <h1 style={{ color: "skyBlue", textAlign: "center", paddingBottom: 15 }}>
+        Grand Master Oogway
+      </h1>
+      <p>
+        He was the previous senior master of the Jade Palace. He is credited as
+        the founder of the Valley of Peace, the creator of kung fu, and the
+        developer of the Dragon Warrior legend. Highly venerated for his wisdom,
+        knowledge, and experience, Oogway was considered by many to be a sage.
+      </p>
+    </div>
+  );
+}
